bd66968bcaf5008a2e359b188cd70a807fc9c5cb
UsadJaydip <114413179+UsadJaydip@users.noreply.github.com>
2024-03-23 08:19:50 +0530

    Add files via upload

---
+++ AbstracrClass.java
@@ -0,0 +1,26 @@
+package com.core;
+
+abstract class Abs1
+{
+	void meth1()
+	{
+		System.out.println("Meth1");
+	}
+	abstract void meth2();
+}
+class Abs2 extends Abs1
+{
+	void meth2()
+	{
+		System.out.println("Meth2");
+    }
+}
+public class AbstracrClass {
+	
+	public static void main(String[] args) {
+		Abs2 a=new Abs2();
+		a.meth1();
+		a.meth2();
+	}
+
+}
